4507f31d92e6076f92f606b83429839a5cfbd404
PQhuyz <86834653+PQhuyz@users.noreply.github.com>
2021-11-09 06:52:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,4 +13,5 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘',
+    messLink: 'http://fb.com' // https://m.me/itachiinohope',
 }
